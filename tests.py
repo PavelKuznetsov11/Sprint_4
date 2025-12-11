92adf989a4ca198fb78160e134ad0734582f9cc0
Pavel Kuznetsov <pavkuznecov@yandex.ru>
2025-12-11 15:37:34 +0300

    Tests

--- tests.py
+++ tests.py
@@ -6,19 +6,13 @@ class TestBooksCollector:
 
 
     def test_add_new_book_add_two_books_is_added(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.books_genre) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
     add_books_negative_tests = [
         ['Как завоевывать друзей и оказывать влияние на людей',
